5814228d4067521218a2ece2b49d684d56d83136
Chaitanya <chaitanyapc9@gmail.com>
2026-02-24 13:33:14 +0530

    Changes

--- Installation.sh
+++ Installation.sh
@@ -5,7 +5,7 @@ echo " User id fomr the id command $UserId"
 
 if [ -z $1 ]; then
   echo " Please pass the required software to install"
-elif [ $# -gt > 1 ]; then
+elif [ $# -gt 1 ]; then
   echo " please pass only one software to install at one time"
   exit 1
 elif [ $UserId -eq 0 ] ; then
