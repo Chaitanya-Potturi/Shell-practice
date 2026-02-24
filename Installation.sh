946c6908fe2c363b2cf7eab3944fab7a76f974bb
Chaitanya <chaitanyapc9@gmail.com>
2026-02-24 12:53:48 +0530

    Changes

---
+++ Installation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+UserId=$(id -u)
+
+if UserId eq 0; then
+  echo "User is root, So proceed with the isntallation"
+  echo " Installing $1"
+  dnf install $1
+else 
+  echo "User is $(whoami) User is not root, Hence skipping installation"
