8ac5ad95baf18c594710613c044c257a55d1a98a
Chaitanya <chaitanyapc9@gmail.com>
2026-02-24 13:06:33 +0530

    Changes

--- Installation.sh
+++ Installation.sh
@@ -3,7 +3,7 @@
 UserId=$(id -u)
 echo " User id fomr the id command $UserId"
 
-if [ UserId -eq 0 ] ; then
+if [ $UserId -eq 0 ] ; then
   echo "User is root, So proceed with the isntallation"
   echo " Installing $1"
   dnf install $1
