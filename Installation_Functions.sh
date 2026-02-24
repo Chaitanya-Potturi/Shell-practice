da92eaee97bb2dead3a4cfd50db95ebd71991b04
Chaitanya <chaitanyapc9@gmail.com>
2026-02-24 19:58:45 +0530

    Updated files

---
+++ Installation_Functions.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#COLOURS
+Red=$(\e[31m)
+Green=$(\e[32m)
+Blue=$(\e[34m)
+yellow=$(\e[33m)
+Na=$(\e[om)
+if [ $# -eq 0 ]; then
+  echo "Please pass the required packages/software to install as Arguments"
+  exit 1
+fi
+
+if [ $(id -u) -ne 0]; then
+  echo "User is not root Hence skipping the installtion"
+  exit 1
+else 
+  Software_Install $@
+fi
+
+Software_Install() {
+
+#Checking if its already installed or not
+
+  for args in $@
+  do
+    pkg=$(rpm -q $@)
+    if [ $? -eq 0 ]; then
+      echo -e "$Green The Package $args is already installed $Na "
+    else 
+      echo -e "$Yellow Installing $args $Na"
+      dnf install $args
+      echo "$Green Package $args Installed $Na"
+  done 
+}
