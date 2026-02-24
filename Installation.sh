eab02fb4c6db43ef3dbc7933a36015563a64c1e3
Chaitanya <chaitanyapc9@gmail.com>
2026-02-24 16:17:24 +0530

    Changes

--- Installation.sh
+++ Installation.sh
@@ -5,13 +5,17 @@ echo " User id form the id command $UserId"
 
 if [ -z $1 ]; then
   echo " Please pass the required software to install"
-elif [ $# -gt 1 ]; then
-  echo " please pass only one software to install at one time"
-  exit 1
-elif [ $UserId -eq 0 ] ; then
-  echo "User is root, So proceed with the isntallation"
-  echo " Installing $1"
-  dnf install $1 -y 
-else 
-  echo "User is $(whoami) is not root user, Hence skipping installation"
+fi
+else
+  for args in $@
+    # elif [ $# -gt 1 ]; then
+    #   echo " please pass only one software to install at one time"
+    #   exit 1
+    if [ $UserId -eq 0 ] ; then
+      echo "User is root, So proceed with the isntallation"
+      echo " Installing $@"
+      dnf install $@ -y 
+    elif 
+      echo "User is $(whoami) is not root user, Hence skipping installation"
+    fi
 fi
