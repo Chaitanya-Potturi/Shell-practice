b3324bc9a96cbf3fe6c1a627856bea3b71efb8d1
Chaitanya <chaitanyapc9@gmail.com>
2026-02-24 20:36:54 +0530

    Updated files

--- Functions.sh
+++ Functions.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+Red='\e[31m'
+Green='\e[32m'
+Blue='\e[34m'
+Yellow='\e[33m'
+Na='\e[0m'
+
 Software_Install() {
 
   for args in $@
-  do
-    dnf install $args
+    do 
+      pkg=$(rpm -q $args )
+      if [ $? -ne 0 ]
+          echo -e "$Red Pacakage is not there $Na"
+          echo -e "$Blue Package $args is getting installed $Na"
+          dnf install $args -y 
+          echo -e "$Green Package is installed $Na"
+      else 
+        echo -e "$Green $args is already installed $Na "
+    
   done 
 }
 
