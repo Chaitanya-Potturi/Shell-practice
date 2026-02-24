af5566ff941dc293b908ee971019f9561e5dc17d
Chaitanya <chaitanyapc9@gmail.com>
2026-02-24 17:26:31 +0530

    Changes

--- Installation.sh
+++ Installation.sh
@@ -10,17 +10,18 @@ fi
 if [ $UserId -eq 0 ]; then
     echo "User is not root, So skipping the isntallation"
     exit 1
+elif 
+  for args in $@
+    do
+      # elif [ $# -gt 1 ]; then
+      #   echo " please pass only one software to install at one time"
+      #   exit 1
+        software=$(rpm -q "$args")
+        if [ $? -eq 0 ]; then
+          echo "$args is already installed"  
+        else
+          echo " Installing $args as it was not installed"
+          dnf install $args -y 
+        fi
+  done
 fi
-for args in $@
-  do
-    # elif [ $# -gt 1 ]; then
-    #   echo " please pass only one software to install at one time"
-    #   exit 1
-      software=$(rpm -q "$args")
-      if [ $? -eq 0 ]; then
-        echo "$args is already installed"  
-      else
-        echo " Installing $args as it was not installed"
-        dnf install $args -y 
-      fi
- done
